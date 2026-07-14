c6394962277be3297bd71937fffb5e1914d68c16
Polper13 <KStachowcz@gmail.com>
2026-07-14 13:06:48 +0200

    init

---
+++ include/engineInfo.h
@@ -0,0 +1,4 @@
+#pragma once
+
+#define ENGINE_NAME "chess-engine"
+#define ENGINE_AUTHOR "Polper13"
---
+++ source/main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+#include "engineInfo.h"
+
+int main()
+{
+    std::cout << "hi :3" << std::endl;
+    return 0;
+}
