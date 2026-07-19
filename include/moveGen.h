4239181f58145905212816d639e865ceb5488ae6
Polper13 <KStachowcz@gmail.com>
2026-07-19 17:25:41 +0200

    generating pawn moves

---
+++ include/moveGen.h
@@ -0,0 +1,23 @@
+#pragma once
+
+#include <vector>
+
+#include "board.h"
+
+struct Move
+{
+    int from;
+    int to;
+    enum Piece promotion = EMPTY;
+    bool isCastle = false;
+    bool isEnPassant = false;
+
+    void print();
+};
+
+std::vector<Move> generatePseudoLegalMoves(Board& board);
+std::vector<Move> generateLegalMoves(Board& board);
+
+void generatePawnMoves(Board& board, int square, std::vector<Move>& moves);
+
+bool isEnemyPiece(Piece piece, bool isWhiteFriendly);
---
+++ source/moveGen.cpp
@@ -0,0 +1,115 @@
+#include <iostream>
+
+#include "moveGen.h"
+
+void Move::print()
+{
+    char fromRank = '1' + from / 8;
+    char fromFile = 'a' + from % 8;
+    char toRank = '1' + to / 8;
+    char toFile = 'a' + to % 8;
+
+    char promotionChar = pieceToChar(promotion);
+    char castleChar = (isCastle) ? 'y' : 'n';
+    char enPassChar = (isEnPassant) ? 'y' : 'n';
+
+    std::cout << fromFile << fromRank << toFile << toRank << " ( promotion: " << promotionChar << " castle: " << castleChar << " enpassant: " << enPassChar << " )" << std::endl;
+}
+
+void generatePawnMoves(Board& board, int square, std::vector<Move>& moves)
+{
+    bool isWhite = board.squares[square] == W_PAWN;
+    int from = square;
+    int to;
+
+    int rank = square / 8 + 1;
+    int file = square % 8 + 1;
+    bool isStarting = (isWhite && rank == 2) || (!isWhite && rank == 7);
+
+    // forward
+    to = (isWhite) ? square + 8 : square - 8;
+    if (board.squares[to] == EMPTY) // if empty in front of him
+    {
+
+        if ((isWhite && rank == 7) || (!isWhite && rank == 2)) // if at the edge of the board
+        {
+            moves.push_back({from, to, (isWhite) ? W_QUEEN : B_QUEEN});
+            moves.push_back({from, to, (isWhite) ? W_ROOK : B_ROOK});
+            moves.push_back({from, to, (isWhite) ? W_BISHOP : B_BISHOP});
+            moves.push_back({from, to, (isWhite) ? W_KNIGHT : B_KNIGHT});
+        }
+        else // just regular move forward
+        {
+            moves.push_back({from, to});
+        }
+
+        if (isStarting && board.squares[isWhite ? to + 8 : to - 8] == EMPTY) // double forward move
+        {
+            moves.push_back({from, (isWhite) ? to + 8 : to - 8});
+        }
+    }
+
+    // left
+    to = (isWhite) ? square + 7 : square - 9;
+    if (file != 1)
+    {
+        if (board.squares[to] != EMPTY && isEnemyPiece(board.squares[to], isWhite))
+        {
+            if ((isWhite && rank == 7) || (!isWhite && rank == 2)) // if at the edge of the board
+            {
+                moves.push_back({from, to, (isWhite) ? W_QUEEN : B_QUEEN});
+                moves.push_back({from, to, (isWhite) ? W_ROOK : B_ROOK});
+                moves.push_back({from, to, (isWhite) ? W_BISHOP : B_BISHOP});
+                moves.push_back({from, to, (isWhite) ? W_KNIGHT : B_KNIGHT});   
+            }
+            else
+            {
+                moves.push_back({from, to});
+            }
+        }
+        else if (to == board.enPassantSquare)
+        {
+            moves.push_back({from, to, EMPTY, false, true});
+        }
+    }
+
+    // right
+    to = (isWhite) ? square + 9 : square - 7;
+    if (file != 8)
+    {
+        if (board.squares[to] != EMPTY && isEnemyPiece(board.squares[to], isWhite))
+        {
+            if ((isWhite && rank == 7) || (!isWhite && rank == 2)) // if at the edge of the board
+            {
+                moves.push_back({from, to, (isWhite) ? W_QUEEN : B_QUEEN});
+                moves.push_back({from, to, (isWhite) ? W_ROOK : B_ROOK});
+                moves.push_back({from, to, (isWhite) ? W_BISHOP : B_BISHOP});
+                moves.push_back({from, to, (isWhite) ? W_KNIGHT : B_KNIGHT});   
+            }
+            else
+            {
+                moves.push_back({from, to});
+            }
+        }
+        else if (to == board.enPassantSquare)
+        {
+            moves.push_back({from, to, EMPTY, false, true});
+        }
+    }
+}
+
+bool isEnemyPiece(Piece piece, bool isWhiteFriendly)
+{
+    if (piece == EMPTY) return false;
+
+    if (isWhiteFriendly && piece >= 7)
+    {
+        return true;
+    }
+    else if (!isWhiteFriendly && piece <= 6)
+    {
+        return true;
+    }
+
+    return false;
+}
